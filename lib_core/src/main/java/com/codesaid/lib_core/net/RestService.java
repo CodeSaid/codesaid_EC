19c4cf9681fa45e32ad4daf145f2d92262bdfabc
CodeSaid <codesaid526@gmail.com>
2020-03-18 21:41:23 +0800

    RestService

---
+++ lib_core/src/main/java/com/codesaid/lib_core/net/RestService.java
@@ -0,0 +1,49 @@
+package com.codesaid.lib_core.net;
+
+import java.util.Map;
+
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Part;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Streaming;
+import retrofit2.http.Url;
+
+/**
+ * Created By codesaid
+ * On :2020-03-18 21:33
+ * Package Name: com.codesaid.lib_core.net
+ * desc: 网络 请求 接口
+ */
+public interface RestService {
+
+    @GET
+    Call<String> get(@Url String url, @QueryMap Map<String, Object> params);
+
+    @FormUrlEncoded
+    @POST
+    Call<String> post(@Url String url, @FieldMap Map<String, Object> params);
+
+    @FormUrlEncoded
+    @PUT
+    Call<String> put(@Url String url, @FieldMap Map<String, Object> params);
+
+    @DELETE
+    Call<String> delete(@Url String url, @QueryMap Map<String, Object> params);
+
+    @Streaming
+    @GET
+    Call<ResponseBody> download(@Url String url, @QueryMap Map<String, Object> params);
+
+    @Multipart
+    @POST
+    Call<String> upload(@Url String url, @Part MultipartBody.Part file);
+}
