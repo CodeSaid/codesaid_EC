f6a599e38ebb04a64f4c1cff41152e57463248ab
CodeSaid <codesaid526@gmail.com>
2020-03-21 21:02:30 +0800

    LoaderStyle

---
+++ lib_core/src/main/java/com/codesaid/lib_core/ui/MyLoader.java
@@ -0,0 +1,17 @@
+package com.codesaid.lib_core.ui;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatDialog;
+
+/**
+ * Created By codesaid
+ * On :2020-03-21 20:53
+ * Package Name: com.codesaid.lib_core.ui
+ * desc:
+ */
+public class MyLoader {
+
+    public static void showLoading(Context context) {
+        final AppCompatDialog dialog = new AppCompatDialog(context);
+    }
+}
