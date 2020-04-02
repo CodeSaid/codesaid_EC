2e9b75378abf669ccd717b9543833b018450ad5e
CodeSaid <codesaid526@gmail.com>
2020-04-02 20:40:10 +0800

    启动页更换

--- app/src/main/java/com/codesaid/MainActivity.java
+++ app/src/main/java/com/codesaid/MainActivity.java
@@ -2,11 +2,12 @@ package com.codesaid;
 
 import com.codesaid.lib_core.activitys.ProxyActivity;
 import com.codesaid.lib_core.delegates.CodeSaidDelegate;
+import com.codesaid.lib_ec.launcher.LauncherDelegate;
 
 public class MainActivity extends ProxyActivity {
 
     @Override
     public CodeSaidDelegate setRootDelegate() {
-        return new MainDelegate();
+        return new LauncherDelegate();
     }
 }
--- lib_core/src/main/java/com/codesaid/lib_core/delegates/BaseDelegate.java
+++ lib_core/src/main/java/com/codesaid/lib_core/delegates/BaseDelegate.java
@@ -1,11 +1,12 @@
 package com.codesaid.lib_core.delegates;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.codesaid.lib_core.activitys.ProxyActivity;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -28,21 +29,25 @@ public abstract class BaseDelegate extends SwipeBackFragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = null;
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View rootView;
 
         if (setLayout() instanceof Integer) {
-            rootView = inflater.inflate((Integer) setLayout(), container, false);
+            rootView = inflater.inflate((int) setLayout(), container, false);
         } else if (setLayout() instanceof View) {
             rootView = (View) setLayout();
+        } else {
+            throw new ClassCastException("setLayout() type must be int or View!");
         }
 
-        if (rootView != null) {
-            mUnbinder = ButterKnife.bind(this, rootView);
-            onBindView(savedInstanceState, rootView);
-        }
+        mUnbinder = ButterKnife.bind(this, rootView);
+        onBindView(savedInstanceState, rootView);
 
         return rootView;
+    }
+
+    public final ProxyActivity getProxyActivity() {
+        return (ProxyActivity) _mActivity;
     }
 
     @Override
