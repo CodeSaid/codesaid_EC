f0170767b3b6e25bed35b3942d99da6ee74ce9e9
CodeSaid <codesaid526@gmail.com>
2020-04-05 22:07:21 +0800

    启动页完善

--- app/src/main/java/com/codesaid/MainActivity.java
+++ app/src/main/java/com/codesaid/MainActivity.java
@@ -1,13 +1,26 @@
 package com.codesaid;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+
 import com.codesaid.lib_core.activitys.ProxyActivity;
 import com.codesaid.lib_core.delegates.CodeSaidDelegate;
-import com.codesaid.lib_ec.launcher.LauncherScrollDelegate;
+import com.codesaid.lib_ec.launcher.LauncherDelegate;
 
 public class MainActivity extends ProxyActivity {
 
     @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+    }
+
+    @Override
     public CodeSaidDelegate setRootDelegate() {
-        return new LauncherScrollDelegate();
+        return new LauncherDelegate();
     }
 }
---
+++ lib_core/src/main/java/com/codesaid/lib_core/ui/launcher/ScrollLauncherTag.java
@@ -0,0 +1,11 @@
+package com.codesaid.lib_core.ui.launcher;
+
+/**
+ * Created By codesaid
+ * On :2020-04-05 21:58
+ * Package Name: com.codesaid.lib_core.ui.launcher
+ * desc:
+ */
+public enum ScrollLauncherTag {
+    HAS_FIRST__LAUNCHER_APP
+}
