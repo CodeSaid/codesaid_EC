0fcda39aad724f39d97bf18df2e5eddaa4e6ee81
CodeSaid <codesaid526@gmail.com>
2020-04-10 19:55:59 +0800

    验证用户输入的内容格式是否正确

--- lib_ec/src/main/java/com/codesaid/lib_ec/sign/SignUpDelegate.java
+++ lib_ec/src/main/java/com/codesaid/lib_ec/sign/SignUpDelegate.java
@@ -2,22 +2,88 @@ package com.codesaid.lib_ec.sign;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.util.Patterns;
 import android.view.View;
 
 import com.codesaid.lib_core.delegates.CodeSaidDelegate;
 import com.codesaid.lib_ec.R;
+import com.codesaid.lib_ec.R2;
+
+import butterknife.BindView;
 
 /**
  * Created By codesaid
  * On :2020-04-07 21:02
  * Package Name: com.codesaid.lib_ec.sign
- * desc:
+ * desc: 注册
  */
 public class SignUpDelegate extends CodeSaidDelegate {
 
+    @BindView(R2.id.edit_sign_up_name)
+    TextInputEditText mName = null;
+    @BindView(R2.id.edit_sign_up_email)
+    TextInputEditText mEmail = null;
+    @BindView(R2.id.edit_sign_up_phone)
+    TextInputEditText mPhone = null;
+    @BindView(R2.id.edit_sign_up_password)
+    TextInputEditText mPassword = null;
+    @BindView(R2.id.edit_sign_up_re_password)
+    TextInputEditText mRePassword = null;
+
+    /**
+     * 验证用户输入的内容格式是否正确
+     */
+    private boolean checkForm() {
+        final String name = mName.getText().toString();
+        final String email = mEmail.getText().toString();
+        final String phone = mPhone.getText().toString();
+        final String password = mPassword.getText().toString();
+        final String rePassword = mRePassword.getText().toString();
+
+        boolean isPass = true;
+
+        if (name.isEmpty()) {
+            mName.setError("请输入姓名");
+            isPass = false;
+        } else {
+            mName.setError(null);
+        }
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmail.setError("错误的邮箱格式");
+            isPass = false;
+        } else {
+            mEmail.setError(null);
+        }
+
+        if (phone.isEmpty() || phone.length() != 11) {
+            mPhone.setError("手机号码错误");
+            isPass = false;
+        } else {
+            mPhone.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 6) {
+            mPassword.setError("请填写至少6位数密码");
+            isPass = false;
+        } else {
+            mPassword.setError(null);
+        }
+
+        if (rePassword.isEmpty() || rePassword.length() < 6 || !(rePassword.equals(password))) {
+            mRePassword.setError("密码验证错误");
+            isPass = false;
+        } else {
+            mRePassword.setError(null);
+        }
+
+        return isPass;
+    }
+
     @Override
     public Object setLayout() {
-        return R.layout.delegate_sing_in;
+        return R.layout.delegate_sing_up;
     }
 
     @Override
