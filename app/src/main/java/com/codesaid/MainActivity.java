62d42687127e67c9d5fc2212a91d864a2e07edec
CodeSaid <codesaid526@gmail.com>
2020-04-14 20:42:02 +0800

    轮播图启动到登录注册逻辑整合

--- app/src/main/java/com/codesaid/MainActivity.java
+++ app/src/main/java/com/codesaid/MainActivity.java
@@ -7,10 +7,13 @@ import android.widget.Toast;
 
 import com.codesaid.lib_core.activitys.ProxyActivity;
 import com.codesaid.lib_core.delegates.CodeSaidDelegate;
+import com.codesaid.lib_core.ui.launcher.ILauncherListener;
+import com.codesaid.lib_core.ui.launcher.OnLauncherFinishTag;
+import com.codesaid.lib_ec.launcher.LauncherDelegate;
 import com.codesaid.lib_ec.sign.ISignListener;
-import com.codesaid.lib_ec.sign.SignUpDelegate;
+import com.codesaid.lib_ec.sign.SignInDelegate;
 
-public class MainActivity extends ProxyActivity implements ISignListener {
+public class MainActivity extends ProxyActivity implements ILauncherListener, ISignListener {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,16 +26,32 @@ public class MainActivity extends ProxyActivity implements ISignListener {
 
     @Override
     public CodeSaidDelegate setRootDelegate() {
-        return new SignUpDelegate();
+        return new LauncherDelegate();
     }
 
     @Override
     public void onSignInSuccess() {
-
+        Toast.makeText(this, "登录成功", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onSignUpSuccess() {
         Toast.makeText(this, "注册成功", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLauncherFinish(OnLauncherFinishTag tag) {
+        switch (tag) {
+            case SIGNED:
+                Toast.makeText(this, "启动结束，用户登录", Toast.LENGTH_SHORT).show();
+                startWithPop(new MainDelegate());
+                break;
+            case NOT_SIGNED:
+                Toast.makeText(this, "启动结束，用户未登录", Toast.LENGTH_SHORT).show();
+                startWithPop(new SignInDelegate());
+                break;
+            default:
+                break;
+        }
     }
 }
