1132d8fcf16d5f052f6c38bb8623f6b4b2e735c1
CodeSaid <codesaid526@gmail.com>
2020-04-14 20:41:10 +0800

    轮播图启动结束接口回调

---
+++ lib_core/src/main/java/com/codesaid/lib_core/ui/launcher/ILauncherListener.java
@@ -0,0 +1,12 @@
+package com.codesaid.lib_core.ui.launcher;
+
+/**
+ * Created By codesaid
+ * On :2020-04-14 20:25
+ * Package Name: com.codesaid.lib_core.ui.launcher
+ * desc:
+ */
+public interface ILauncherListener {
+
+    void onLauncherFinish(OnLauncherFinishTag tag);
+}
---
+++ lib_core/src/main/java/com/codesaid/lib_core/ui/launcher/OnLauncherFinishTag.java
@@ -0,0 +1,12 @@
+package com.codesaid.lib_core.ui.launcher;
+
+/**
+ * Created By codesaid
+ * On :2020-04-14 20:26
+ * Package Name: com.codesaid.lib_core.ui.launcher
+ * desc:
+ */
+public enum  OnLauncherFinishTag {
+    SIGNED,
+    NOT_SIGNED
+}
