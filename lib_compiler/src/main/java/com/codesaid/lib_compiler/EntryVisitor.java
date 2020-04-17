48304b266f5a491c079de8c40a361a5ab331339f
CodeSaid <codesaid526@gmail.com>
2020-04-17 21:32:20 +0800

    微信 compiler

---
+++ lib_compiler/src/main/java/com/codesaid/lib_compiler/EntryVisitor.java
@@ -0,0 +1,62 @@
+package com.codesaid.lib_compiler;
+
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
+
+import java.io.IOException;
+
+import javax.annotation.processing.Filer;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.SimpleAnnotationValueVisitor7;
+
+/**
+ * Created By codesaid
+ * On :2020-04-17 21:21
+ * Package Name: com.codesaid.lib_compiler
+ * desc:
+ */
+public final class EntryVisitor extends SimpleAnnotationValueVisitor7<Void, Void> {
+
+    private Filer mFiler = null;
+    private TypeMirror mTypeMirror = null;
+    private String mPackageName = null;
+
+    public void setFiler(Filer filer) {
+        mFiler = filer;
+    }
+
+    @Override
+    public Void visitString(String s, Void aVoid) {
+        mPackageName = s;
+        return aVoid;
+    }
+
+    @Override
+    public Void visitType(TypeMirror typeMirror, Void aVoid) {
+        mTypeMirror = typeMirror;
+        generateJavaCode(typeMirror);
+        return aVoid;
+    }
+
+    private void generateJavaCode(TypeMirror typeMirror) {
+        final TypeSpec targetActivity =
+                TypeSpec.classBuilder("WXEntryActivity")
+                        .addModifiers(Modifier.PUBLIC)
+                        .addModifiers(Modifier.FINAL)
+                        .superclass(TypeName.get(typeMirror))
+                        .build();
+
+        final JavaFile javaFile =
+                JavaFile.builder(mPackageName + ".wxapi", targetActivity)
+                        .addFileComment("微信入口文件")
+                        .build();
+
+        try {
+            javaFile.writeTo(mFiler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
