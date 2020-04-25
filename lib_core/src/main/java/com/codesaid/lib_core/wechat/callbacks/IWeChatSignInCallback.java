940c3ed0136ab3c3a7ac629de0b7282b4ddcb633
CodeSaid <codesaid526@gmail.com>
2020-04-25 22:58:19 +0800

    wechat

---
+++ lib_core/src/main/java/com/codesaid/lib_core/wechat/callbacks/IWeChatSignInCallback.java
@@ -0,0 +1,12 @@
+package com.codesaid.lib_core.wechat.callbacks;
+
+/**
+ * Created By codesaid
+ * On :2020-04-22 21:06
+ * Package Name: com.codesaid.lib_core.wechat.callbacks
+ * desc:
+ */
+public interface IWeChatSignInCallback {
+
+    void onSignInSuccess(String userInfo);
+}
