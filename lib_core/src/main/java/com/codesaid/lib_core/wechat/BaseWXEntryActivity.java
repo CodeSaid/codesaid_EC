c916abc5f87bb716c7acc157ff42e255da38babc
CodeSaid <codesaid526@gmail.com>
2020-04-28 21:15:46 +0800

    wechat

--- lib_core/src/main/java/com/codesaid/lib_core/wechat/BaseWXEntryActivity.java
+++ lib_core/src/main/java/com/codesaid/lib_core/wechat/BaseWXEntryActivity.java
@@ -1,5 +1,16 @@
 package com.codesaid.lib_core.wechat;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.codesaid.lib_core.net.RestClient;
+import com.codesaid.lib_core.net.callback.IError;
+import com.codesaid.lib_core.net.callback.IFailure;
+import com.codesaid.lib_core.net.callback.ISuccess;
+import com.codesaid.lib_core.utils.log.MyLogger;
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.modelmsg.SendAuth;
+
 /**
  * Created By codesaid
  * On :2020-04-18 21:28
@@ -15,5 +26,91 @@ public abstract class BaseWXEntryActivity extends BaseWXActivity {
      */
     protected abstract void onSignInSuccess(String userInfo);
 
-    
+    /**
+     * 微信发送请求到第三方应用后的回调
+     *
+     * @param baseReq
+     */
+    @Override
+    public void onReq(BaseReq baseReq) {
+
+    }
+
+    /**
+     * 第三方应用发送请求到微信后的回调
+     *
+     * @param baseResp
+     */
+    @Override
+    public void onResp(BaseResp baseResp) {
+        // 获取 code
+        final String code = ((SendAuth.Resp) baseResp).code;
+
+        final StringBuilder authUrl = new StringBuilder();
+        authUrl
+                .append("https://api.weixin.qq.com/sns/oauth2/access_token?appid=")
+                .append(MyWeChat.APP_ID)
+                .append("&secret=")
+                .append(MyWeChat.APP_SECRET)
+                .append("&code=")
+                .append(code)
+                .append("&grant_type=authorization_code");
+
+        MyLogger.d("authUrl", authUrl.toString());
+        getAuth(authUrl.toString());
+    }
+
+    private void getAuth(String authUrl) {
+        RestClient
+                .builder()
+                .url(authUrl)
+                .success(new ISuccess() {
+                    @Override
+                    public void onSuccess(String response) {
+                        final JSONObject authObj = JSON.parseObject(response);
+                        final String accessToken = authObj.getString("access_token");
+                        final String openId = authObj.getString("openid");
+
+                        final StringBuilder userInfoUrl = new StringBuilder();
+                        userInfoUrl
+                                .append("https://api.weixin.qq.com/sns/userinfo?access_token=")
+                                .append(accessToken)
+                                .append("&openid=")
+                                .append(openId)
+                                .append("&lang=")
+                                .append("zh_CN");
+
+                        MyLogger.d("userInfoUrl", userInfoUrl.toString());
+                        getUserInfo(userInfoUrl.toString());
+                    }
+                })
+                .build()
+                .get();
+    }
+
+    private void getUserInfo(String userInfoUrl) {
+        RestClient
+                .builder()
+                .url(userInfoUrl)
+                .success(new ISuccess() {
+                    @Override
+                    public void onSuccess(String response) {
+                        onSignInSuccess(response);
+                    }
+                })
+                .failure(new IFailure() {
+                    @Override
+                    public void onFailure() {
+
+                    }
+                })
+                .error(new IError() {
+                    @Override
+                    public void onError(int code, String msg) {
+
+                    }
+                })
+                .build()
+                .get();
+    }
 }
