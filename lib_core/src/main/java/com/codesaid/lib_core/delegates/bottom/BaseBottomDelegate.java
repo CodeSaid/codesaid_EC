89a1284bb5d55e80a231460a2d78ec9b1636d3b8
CodeSaid <codesaid526@gmail.com>
2020-04-30 21:46:52 +0800

    BaseBottomDelegate 底部 icon 和点击事件

--- lib_core/src/main/java/com/codesaid/lib_core/delegates/bottom/BaseBottomDelegate.java
+++ lib_core/src/main/java/com/codesaid/lib_core/delegates/bottom/BaseBottomDelegate.java
@@ -4,12 +4,23 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
 
+import com.codesaid.lib_core.R;
+import com.codesaid.lib_core.R2;
 import com.codesaid.lib_core.delegates.CodeSaidDelegate;
+import com.joanzapata.iconify.widget.IconTextView;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
+import butterknife.BindView;
+import me.yokeyword.fragmentation.SupportFragment;
 
 /**
  * Created By codesaid
@@ -17,18 +28,21 @@ import java.util.Map;
  * Package Name: com.codesaid.lib_core.delegates.bottom
  * desc:
  */
-public abstract class BaseBottomDelegate extends CodeSaidDelegate {
+public abstract class BaseBottomDelegate extends CodeSaidDelegate implements View.OnClickListener {
 
     private final ArrayList<BottomItemDelegate> ITEM_DELEGATES = new ArrayList<>();
     private final ArrayList<BottomTabBean> TAB_BEANS = new ArrayList<>();
     private final LinkedHashMap<BottomTabBean, BottomItemDelegate> ITEMS = new LinkedHashMap<>();
 
-    // 计数
+    // 当前的 delegate
     private int mCurrentDelegate = 0;
     // 默认展示的 delegate
     private int mIndexDelegate = 0;
     // icon 点击之后改变的颜色
     private int mClickColor = Color.RED;
+
+    @BindView(R2.id.bottom_bar)
+    LinearLayoutCompat mBottomBar = null;
 
     public abstract LinkedHashMap<BottomTabBean, BottomItemDelegate> setItems(ItemBuilder builder);
 
@@ -55,5 +69,65 @@ public abstract class BaseBottomDelegate extends CodeSaidDelegate {
             TAB_BEANS.add(key);
             ITEM_DELEGATES.add(value);
         }
+    }
+
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_bottom;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+        final int size = ITEMS.size();
+        for (int i = 0; i < size; i++) {
+            LayoutInflater.from(getContext()).inflate(R.layout.bottom_item_icon_text_layout, mBottomBar);
+            final RelativeLayout item = (RelativeLayout) mBottomBar.getChildAt(i);
+            // 设置每个 item 的点击事件
+            item.setTag(i);
+            item.setOnClickListener(this);
+            final IconTextView itemIcon = (IconTextView) item.getChildAt(0);
+            final AppCompatTextView itemTitle = (AppCompatTextView) item.getChildAt(1);
+            final BottomTabBean bean = TAB_BEANS.get(i);
+            // 初始化 数据
+            itemIcon.setText(bean.getIcon());
+            itemTitle.setText(bean.getTitle());
+
+            // 设置当前 显示的 icon 的颜色
+            if (i == mIndexDelegate) {
+                itemIcon.setTextColor(mClickColor);
+                itemTitle.setTextColor(mClickColor);
+            }
+        }
+
+        final SupportFragment[] delegateArray = ITEM_DELEGATES.toArray(new SupportFragment[size]);
+        loadMultipleRootFragment(R.id.bottom_bar_delegate_container, mIndexDelegate, delegateArray);
+    }
+
+    /**
+     * 重置 icon 的颜色
+     */
+    private void resetColor() {
+        final int count = mBottomBar.getChildCount();
+        for (int i = 0; i < count; i++) {
+            RelativeLayout item = (RelativeLayout) mBottomBar.getChildAt(i);
+            IconTextView itemIcon = (IconTextView) item.getChildAt(0);
+            itemIcon.setTextColor(Color.GRAY);
+            AppCompatTextView itemTitle = (AppCompatTextView) item.getChildAt(1);
+            itemTitle.setTextColor(Color.GRAY);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int tag = (int) v.getTag();
+        resetColor();
+        RelativeLayout item = (RelativeLayout) v;
+        IconTextView itemIcon = (IconTextView) item.getChildAt(0);
+        itemIcon.setTextColor(mClickColor);
+        AppCompatTextView itemTitle = (AppCompatTextView) item.getChildAt(1);
+        itemTitle.setTextColor(mClickColor);
+        showHideFragment(ITEM_DELEGATES.get(tag), ITEM_DELEGATES.get(mCurrentDelegate));
+        // 注意先后顺序
+        mCurrentDelegate = tag;
     }
 }
