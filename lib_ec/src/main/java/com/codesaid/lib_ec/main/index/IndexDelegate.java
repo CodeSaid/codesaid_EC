8836246051fffba065887497759ee09cb263e618
CodeSaid <codesaid526@gmail.com>
2020-05-02 20:37:21 +0800

    首页 refresh

--- lib_ec/src/main/java/com/codesaid/lib_ec/main/index/IndexDelegate.java
+++ lib_ec/src/main/java/com/codesaid/lib_ec/main/index/IndexDelegate.java
@@ -2,10 +2,19 @@ package com.codesaid.lib_ec.main.index;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.codesaid.lib_core.delegates.bottom.BottomItemDelegate;
+import com.codesaid.lib_core.ui.refresh.RefreshHandler;
 import com.codesaid.lib_ec.R;
+import com.codesaid.lib_ec.R2;
+import com.joanzapata.iconify.widget.IconTextView;
+
+import butterknife.BindView;
 
 /**
  * Created By codesaid
@@ -15,6 +24,35 @@ import com.codesaid.lib_ec.R;
  */
 public class IndexDelegate extends BottomItemDelegate {
 
+    @BindView(R2.id.rv_index)
+    RecyclerView mRecyclerView = null;
+    @BindView(R2.id.srl_index)
+    SwipeRefreshLayout mRefreshLayout = null;
+    @BindView(R2.id.tb_index)
+    Toolbar mToolbar = null;
+    @BindView(R2.id.icon_index_scan)
+    IconTextView mIconScan = null;
+    @BindView(R2.id.et_search_view)
+    AppCompatEditText mSearchView = null;
+
+    private RefreshHandler mRefreshHandler = null;
+
+
+    private void initRefreshLayout() {
+        mRefreshLayout.setColorSchemeResources(
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light
+        );
+        mRefreshLayout.setProgressViewOffset(true, 120, 300);
+    }
+
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+        initRefreshLayout();
+    }
+
     @Override
     public Object setLayout() {
         return R.layout.delegate_index;
@@ -22,6 +60,6 @@ public class IndexDelegate extends BottomItemDelegate {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
-
+        mRefreshHandler = new RefreshHandler(mRefreshLayout);
     }
 }
